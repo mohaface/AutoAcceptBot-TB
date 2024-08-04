c8168fcba1882059a76a5188e06c8d46cb42b16a
Mr STRATEGIST <142794511+tandavbot2@users.noreply.github.com>
2024-08-04 17:19:13 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -5,7 +5,7 @@ class Config:
     API_HASH = getenv("API_HASH", "")
     BOT_TOKEN = getenv("BOT_TOKEN", "")
     FSUB = getenv("FSUB", "")
-    CHID = int(getenv("CHID", ""))
+    CHID = int(getenv("CHID", "-1002177395485"))
     SUDO = list(map(int, getenv("SUDO", "").split()))
     MONGO_URI = getenv("MONGO_URI", "")
     
