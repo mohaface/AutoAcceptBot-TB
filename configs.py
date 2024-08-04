58cf374ced4e325f786c584e9455e84fb6fc6bd3
Mr STRATEGIST <142794511+tandavbot2@users.noreply.github.com>
2024-08-04 17:15:11 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,15 +1,15 @@
 from os import getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "13602725"))
-    API_HASH = getenv("API_HASH", "1b10ef3c3fc08c5d3134d67d15ff42fc")
-    BOT_TOKEN = getenv("BOT_TOKEN", "7209810551:AAFenA9M5Rqdnf-P0TjNsQNh57GekfFoBqM")
-    FSUB = getenv("FSUB", "or3kii")
-    CHID = int(getenv("CHID", "-1002012969670"))
-    SUDO = list(map(int, getenv("SUDO", "6265954306").split()))
-    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Tandav2:TandavBotsDB@tandav2.qkdxrpn.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=tandav2")
+    API_ID = int(getenv("API_ID", ""))
+    API_HASH = getenv("API_HASH", "")
+    BOT_TOKEN = getenv("BOT_TOKEN", "")
+    FSUB = getenv("FSUB", "")
+    CHID = int(getenv("CHID", ""))
+    SUDO = list(map(int, getenv("SUDO", "").split()))
+    MONGO_URI = getenv("MONGO_URI", "")
     
     # New configuration for required channels
-    REQUIRED_CHANNELS = getenv("REQUIRED_CHANNELS", "@or3kii,@or3kii").split(',')
+    REQUIRED_CHANNELS = getenv("REQUIRED_CHANNELS", "Or3kii").split(',')
 
 cfg = Config()
